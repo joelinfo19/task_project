aa8619268ea0a0128056e9f917f03a94469f9d38
joel <joel_zerg_pro@outlook.com>
2021-11-25 18:55:51 -0500

    first commit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Sidebar=()=>{
+    return(
+        <>
+
+            <h1>
+                Sidebar
+            </h1>
+
+        </>
+    )
+
+}
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Navbar} from "../components/Navbar";
+
+export const HomePage=()=>{
+    return(
+        <div>
+
+            <h1>
+                <Navbar/>
+            </h1>
+
+        </div>
+    )
+
+
+
+}
